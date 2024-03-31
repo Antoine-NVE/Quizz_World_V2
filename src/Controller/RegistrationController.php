5452db3335e7c487af946f36bffa44836fbf0199
Antoine-NVE <antoinenavette@gmail.com>
2024-04-01 00:02:04 +0200

    Ajout de messages flash et lien de renvoi du mail de vérification

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
@@ -54,6 +55,7 @@ class RegistrationController extends AbstractController
                     ->subject('Merci de confirmer ton email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
+            $this->addFlash('success', 'Un email de confirmation a été envoyé à ton adresse.');
 
             // do anything else you need here, like send an email
 
@@ -93,5 +95,22 @@ class RegistrationController extends AbstractController
         $this->addFlash('success', 'Ton adresse email a bien été vérifiée, tu peux maintenant te reconnecter.');
 
         return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/resend-verif', name: 'app_resend_verif')]
+    public function resendVerif(UserInterface $user): Response
+    {
+        $this->emailVerifier->sendEmailConfirmation(
+            'app_verify_email',
+            $user,
+            (new TemplatedEmail())
+                ->from(new Address('no-reply@quizzworld.fr', 'Quizz World Bot'))
+                ->to($user->getEmail())
+                ->subject('Merci de confirmer ton email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+        );
+        $this->addFlash('success', 'Un email de vérification a été envoyé à ton adresse.');
+
+        return $this->redirectToRoute('app_main');
     }
 }
