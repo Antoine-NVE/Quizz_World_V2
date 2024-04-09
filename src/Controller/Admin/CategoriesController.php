7856507eb8f8a1eac5dbf27fc2c0dd7ddbd85c8c
Antoine-NVE <antoinenavette@gmail.com>
2024-04-09 18:07:24 +0200

    Début de l'interface de création des catégories

--- src/Controller/Admin/CategoriesController.php
+++ src/Controller/Admin/CategoriesController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Categories;
+use App\Entity\Questionnaires;
+use App\Form\CategoriesFormType;
 use App\Repository\CategoriesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/categories', name: 'admin_categories_')]
 class CategoriesController extends AbstractController
@@ -16,5 +23,48 @@ class CategoriesController extends AbstractController
         $categories = $categoriesRepository->findAllWithUserAndNumberOfQuestions();
 
         return $this->render('admin/categories/index.html.twig', compact('categories'));
+    }
+
+    #[Route('/ajouter', name: 'add')]
+    public function add(SluggerInterface $slugger, UserInterface $user, EntityManagerInterface $manager, Request $request): Response
+    {
+        $category = new Categories();
+        $form = $this->createForm(CategoriesFormType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category->setSlug($slugger->slug($category->getTitle())->lower());
+            $category->setUser($user);
+
+            // On récupère l'image et l'extension
+            $image = $form->get('image')->getData();
+            $extension = $image->guessExtension();
+
+            // On définie son nom à partir du slug de la catégorie
+            $imageName = $category->getSlug() . '.' . $extension;
+
+            // On la crée dans le dossier public/img/
+            $image->move($this->getParameter('images_directory'), $imageName);
+
+            // On indique à l'objet le nom de l'image
+            $category->setImage($imageName);
+
+            $difficulties = ['facile', 'moyen', 'difficile'];
+            foreach ($difficulties as $difficulty) {
+                $questionnaire = new Questionnaires();
+                $questionnaire->setDifficulty($difficulty);
+                $questionnaire->setCategory($category);
+
+                $manager->persist($questionnaire);
+            }
+
+            $manager->persist($category);
+            $manager->flush();
+
+            // return $this->redirectToRoute('admin_categories_index');
+        }
+
+        return $this->render('admin/categories/add.html.twig', compact('form'));
     }
 }
---
+++ src/Form/CategoriesFormType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categories;
+use App\Entity\Users;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoriesFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'required' => false
+            ])
+            ->add('image', FileType::class, [
+                'required' => false
+            ])
+            ->add('isActive', CheckboxType::class, [
+                'label' => 'Active',
+                'required' => false
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categories::class,
+        ]);
+    }
+}
