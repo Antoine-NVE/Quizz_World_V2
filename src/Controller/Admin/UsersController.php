06aceb6d7cfa499e1600f7afe7d783dec46e00c8
Antoine-NVE <antoinenavette@gmail.com>
2024-04-17 08:36:09 +0200

    Création de la route de suppression des utilisateurs

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Users;
 use App\Form\UsersFormType;
 use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,7 +22,7 @@ class UsersController extends AbstractController
         return $this->render('admin/users/index.html.twig', compact('users'));
     }
 
-    #[Route('/{id}', name: 'edit')]
+    #[Route('/{id}', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(int $id, UsersRepository $usersRepository, EntityManagerInterface $manager, Request $request): Response
     {
         $user = $usersRepository->find($id);
@@ -41,5 +42,15 @@ class UsersController extends AbstractController
         }
 
         return $this->render('admin/users/edit.html.twig', compact('form'));
+    }
+
+    #[Route('/{id}', name: 'edit', methods: ['DELETE'])]
+    public function remove(Users $user, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($user);
+        $manager->flush();
+
+        $this->addFlash('success', 'L\'utilisateur a bien été supprimé.');
+        return $this->redirectToRoute(('admin_users_index'));
     }
 }
