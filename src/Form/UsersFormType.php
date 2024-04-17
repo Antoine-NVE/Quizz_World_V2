aa9dc306c22cc0ae01e6b358ddeea8c85fe150bb
Antoine-NVE <antoinenavette@gmail.com>
2024-04-17 08:20:59 +0200

    Ajout des contraintes de validation

--- src/Form/UsersFormType.php
+++ src/Form/UsersFormType.php
@@ -7,14 +7,40 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UsersFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
+            ->add('pseudo', options: [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner votre pseudo.'
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre pseudo doit faire au minimum {{ limit }} caractères.',
+                        'max' => 20,
+                        'maxMessage' => 'Votre pseudo doit faire au maximum {{ limit }} caractères.'
+                    ]),
+                ]
+            ])
+            ->add('email', options: [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner votre email.'
+                    ]),
+                    new Email([
+                        'message' => 'Email invalide.'
+                    ])
+                ]
+            ])
             ->add('role', options: [
                 'mapped' => false,
                 'disabled' => true,
